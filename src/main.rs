48072518f728fbd14c43106f1d3abea4958d15d8
yskaksk <yskaksk@gmail.com>
2021-07-01 22:18:50 +0900

    step0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use rustmal::step0_repl::rep;
+
+use rustyline::Editor;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rl = Editor::<()>::new();
+    loop {
+        let readline = rl.readline("user> ");
+        match readline {
+            Ok(line) => println!("{}", rep(&line)),
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
 }
---
+++ src/step0_repl.rs
@@ -0,0 +1,15 @@
+fn READ(s: &str) -> &str {
+    return s
+}
+
+fn EVAL(s: &str) -> &str {
+    return s
+}
+
+fn PRINT(s: &str) -> &str {
+    return s
+}
+
+pub fn rep(s: &str) -> &str {
+    return PRINT(EVAL(READ(s)))
+}
