769f8cb24724c98f07b90166463bdac12cbd5357
yskaksk <yskaksk@gmail.com>
2021-07-06 01:23:12 +0900

    fmt

--- src/printer.rs
+++ src/printer.rs
@@ -1,12 +1,11 @@
 use crate::types::MalType;
 use crate::types::MalType::{MalList, MalNumber, MalSymbol};
 
-
 pub fn pr_str(mt: &MalType) -> String {
     match mt {
         MalNumber(val) => format!("{}", val),
         MalSymbol(val) => format!("{}", val),
-        MalList {elm} => {
+        MalList { elm } => {
             let mut s = String::from("(");
             let mut i = 0;
             for el in elm {
--- src/step1_read_print.rs
+++ src/step1_read_print.rs
@@ -1,21 +1,20 @@
+use crate::printer::pr_str;
 use crate::reader::read_str;
 use crate::types::MalType;
-use crate::printer::pr_str;
 
 fn READ(s: &str) -> MalType {
     let r = read_str(s);
-    return r
+    return r;
 }
 
 fn EVAL(mal: MalType) -> MalType {
-    return mal
+    return mal;
 }
 
 fn PRINT(mal: MalType) -> String {
-    return pr_str(&mal)
+    return pr_str(&mal);
 }
 
 pub fn rep(s: &str) -> String {
-    return PRINT(EVAL(READ(s)))
+    return PRINT(EVAL(READ(s)));
 }
-
